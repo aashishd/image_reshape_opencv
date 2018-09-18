85d9886b082a81528ace031ff8d7ac594b804f9f
Ashish Dhiman <ashish.dhiman.nith@gmail.com>
2018-09-18 17:28:35 +0530

    Adding feature to crop instead of pad

--- image_converter.py
+++ image_converter.py
@@ -7,6 +7,7 @@ Created on Mon Sep 17 21:16:48 2018
 """
 
 import cv2
+import random
 
 class ImageShapeConverter(object):
     """
@@ -15,11 +16,12 @@ class ImageShapeConverter(object):
     @param : width = Width of the output image
     """
      
-    def __init__(self, height=360, width=480, maintain_aspect_ratio=True):
+    def __init__(self, height=360, width=600, maintain_aspect_ratio=True, crop_instead_of_pad=False):
         self.HEIGHT = height
         self.WIDTH = width
         self.pad_color = [0, 0, 0] # set default pad color to black
         self.maintain_aspect_ratio = maintain_aspect_ratio # set default value
+        self.crop_instead_of_pad = crop_instead_of_pad
 
     # get image shape from h and width
     def get_out_image_dims(self, h, w, c):
@@ -27,7 +29,13 @@ class ImageShapeConverter(object):
             # if image is smaller than required height and width
             height = h
             width = w
-        elif h/w > self.HEIGHT/self.WIDTH:
+            return (height, width)
+        aspect_ratio_comp = h/w > self.HEIGHT/self.WIDTH
+        
+        if self.crop_instead_of_pad:
+            aspect_ratio_comp = not aspect_ratio_comp
+        
+        if aspect_ratio_comp:
             # height of image is more than width
             height = self.HEIGHT
             width = int((self.HEIGHT/h) * w)
@@ -79,9 +87,32 @@ class ImageShapeConverter(object):
         result = cv2.copyMakeBorder(image,t,b,l,r,cv2.BORDER_CONSTANT,value=pad_color)
         return result
     
+    # pad the image with given color, default pad color is black
+    def random_crop(self, image):
+        h, w, c = image.shape
+        if h > self.HEIGHT:
+            # crop height of image
+            h_diff = h - self.HEIGHT
+            print(h_diff)
+            h_index = random.randint(0, h_diff)
+            print(h_index)
+            image = image[h_index:h_index+self.HEIGHT, :, :] # crop image height
+        if w > self.HEIGHT:
+            # crop width of image
+            w_diff = w - self.WIDTH
+            w_index = random.randint(0, w_diff)
+            image = image[:, w_index:w_index+self.WIDTH, :] # crop image width
+        return image
+    
+    
     # convert the input image to output image format
     def convert(self, image, pad_color=None):
         image = self.resize_image(image)
+        if self.crop_instead_of_pad:
+            return self.random_crop(image)
         return self.pad_out_image(image, pad_color)
-
-
+    
+        # convert the input image to output image format
+    def convert_file(self, image_path, pad_color=None):
+        image = cv2.imread(image_path)
+        return self.convert(image, pad_color)
